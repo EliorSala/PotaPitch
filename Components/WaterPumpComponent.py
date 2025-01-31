33283042cdaa0bfd40176115b07a636a6731b0be
elior <elior.sala@gmail.com>
2025-01-31 11:51:59 +0200

    added pid to algorithm

--- Components/WaterPumpComponent.py
+++ Components/WaterPumpComponent.py
@@ -4,18 +4,21 @@ from Core.PumpBase import PumpBase
 
 
 class WaterPumpComponent(PumpBase):
-    def __init__(self, pump_pin, active_time):
+    def __init__(self, pump_pin, default_active_time):
         """
         Initializes the water pump with a specified active time.
 
-        :param active_time: Duration (in seconds) for which the pump will be active.
+        :param default_active_time: Duration (in seconds) for which the pump will be active.
         """
-        super().__init__(pump_pin, active_time)
+        super().__init__(pump_pin, default_active_time)
 
-    def activate_pump(self):
+    def activate_pump(self, active_time=None):
         """
         Activates the water pump for the duration equal to active_time.
         """
         self.pump_pin.off()
-        time.sleep(self.active_time)
+        if active_time is None:
+            time.sleep(self.default_active_time)
+        else:
+            time.sleep(active_time)
         self.pump_pin.on()
